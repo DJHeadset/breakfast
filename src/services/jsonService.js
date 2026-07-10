ba06093ec79417dc62939574d9bfb23ce4df556c
DJHeadset <zoltan.blaho.83@gmail.com>
2026-07-10 15:36:26 +0200

    link for oldJson fixed

--- src/services/jsonService.js
+++ src/services/jsonService.js
@@ -1,6 +1,6 @@
 export const getOldJson = async () => {
   try {
-    const res = await fetch("http://192.168.0.94:5000/chores/get_chores");
+    const res = await fetch("http://192.168.0.38:5000/chores/get_chores");
     const data = await res.json();
     //console.log(data);
     return data;
